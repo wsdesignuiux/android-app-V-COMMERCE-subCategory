75bd53128af2709104cde2235ac54ec65a4d9ed2
Rushabh Patel <wsdesignuiux@gmail.com>
2018-11-16 09:40:26 +0530

    Initial commit

---
+++ app/src/main/java/adapter/List_listAdapter.java
@@ -0,0 +1,58 @@
+package adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import e.wolfsoft1.v_commerce_subcategory.R;
+import model.PojoClass;
+
+
+public class List_listAdapter extends RecyclerView.Adapter<List_listAdapter.ViewHolder> {
+
+    private Context context;
+    private ArrayList<PojoClass> pojoClassArrayList;
+
+    public List_listAdapter(Context context, ArrayList<PojoClass> pojoClassArrayList) {
+        this.context = context;
+        this.pojoClassArrayList = pojoClassArrayList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_list, parent, false);
+
+        return new ViewHolder(itemView);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        holder.title.setText(pojoClassArrayList.get(position).getTitle());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return pojoClassArrayList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView title;
+        public ViewHolder(View view) {
+            super(view);
+
+            title = view.findViewById(R.id.title);
+        }
+    }
+}
---
+++ app/src/main/java/model/PojoClass.java
@@ -0,0 +1,14 @@
+package model;
+
+public class PojoClass {
+
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
